9cd3f220aca59308081043835a852e2cfda5c491
Elkina Yulia <uelkina@accelcomp.org>
2020-01-10 18:29:24 +0300

    draw line to click

--- src/app/draw/draw.component.ts
+++ src/app/draw/draw.component.ts
@@ -22,13 +22,17 @@ export class DrawComponent implements OnInit {
   private dataPoints: Object[];
   private points: Object[];
   private polygons: Object[];
+  private isNewLine: boolean;
+  private polylines: Object[];
+  private newPolyline: Object;
+  private lengthCoordsNewPolyline: number;
 
   constructor( private _dataService: DataService ) { }
 
   ngOnInit() {
     this.height = 300;
     this.isShowGrid = true;
-    this.isHoverable = true;
+    this.isHoverable = false;
     this.sizePoint = 5;  
     
     this.rCircle = 20;
@@ -63,25 +67,71 @@ export class DrawComponent implements OnInit {
     });
 
     this.polygons = this._dataService.getDataPolygons();
+
+    this.isNewLine = false;
+    this.newPolyline = {};
+    this.lengthCoordsNewPolyline = 0;
+    this.polylines = [];
   }
 
   onClick(evt) {
-    console.log("click! ", evt);
+    console.log("click!", evt);
+  }
+
+  onMouseMove(evt) {
+    if (this.isNewLine) {
+      console.log("mouse move", evt);
+    }
   }
 
   onClickEl(evt) {
-    console.log("click to el ", evt);
+    const el = evt.target;
+    const attrEl = el.attributes;
+    const cxEl = attrEl.getNamedItem('cx').value;
+    const cyEl = attrEl.getNamedItem('cy').value;
+
+    if (!this.isNewLine) {
+      this.isNewLine = true;
+      // создаём новую polyline
+      const id = this.polylines.length;
+      const coords = [[cxEl, cyEl]];
+      const color = "#000000";
+      this.newPolyline = {id, coords, color};
+      this.lengthCoordsNewPolyline = 1;
+    }
+    else {
+      console.log("продолжаем");
+      // продолжаем начатую polyline
+      let arrCoords = this.newPolyline['coords'];
+      const lastCoords = arrCoords[arrCoords.length - 1];
+      if ((lastCoords[0] !== cxEl) || (lastCoords[1] !== cyEl)) {
+        arrCoords.push([cxEl, cyEl]);
+        this.lengthCoordsNewPolyline++;
+      }
+    }
+  }
+
+  onDoubleClickEl(evt) {
+    if (this.isNewLine) {
+      /* завершаем начатую ранее polyline */
+      this.isNewLine = false;
+      const arrCoords = this.newPolyline['coords'];
+      if (arrCoords.length > 1) {
+        this.polylines.push(this.newPolyline);
+        this.newPolyline = {};
+      }
+    }
   }
 
   onMouseOverEl(evt) {
-    console.log("mouse over to the el ", evt);
+    //console.log("mouse over to the el ", evt);
     const el = evt.target;
     /*el.style.stroke="black"
     el.style.strokeWidth="1"*/
   }
 
   onMouseOutEl(evt) {
-    console.log("mouse out to the el ", evt);
+    //console.log("mouse out to the el ", evt);
     const el = evt.target;
     /*el.style.stroke="none"
     el.style.strokeWidth="none"*/
